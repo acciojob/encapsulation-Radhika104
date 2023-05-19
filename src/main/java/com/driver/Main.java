02fc8cf4bce7d1f95c6acee64d3e260ffa0a04fc
Radhika104 <87032257+Radhika104@users.noreply.github.com>
2023-05-19 20:29:30 +0530

    assignment completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
-public class Main {
-  
+import java.util.*;
+public class Main
+{
+public static void main(String args[])
+{
+RWOnly obj=new RWOnly();
+//obj.name="Radhika";
+//System.out.println(obj.name); this will give you error as we cannot access private members directly
+obj.setName("Radhika Agrawal");
+System.out.println(obj.getName());
+}
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,15 @@
 package com.driver;
 
-public class RWOnly {
-    
+import java.util.*;
+public class RWOnly
+{
+private String name;
+public void setName(String  name)
+{
+this.name=name;
+}
+public String getName()
+{
+return this.name;
+}
 }
